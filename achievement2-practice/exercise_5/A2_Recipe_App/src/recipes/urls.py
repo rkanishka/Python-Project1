04536bd6b50959006f1785d894eb3e06417dbb11
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-09-10 14:23:36 -0700

    add login logout

--- achievement2-practice/exercise_5/A2_Recipe_App/src/recipes/urls.py
+++ achievement2-practice/exercise_5/A2_Recipe_App/src/recipes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/', views.RecipeListView.as_view(), name='recipe_list'),
-    path('recipe/<uuid:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
