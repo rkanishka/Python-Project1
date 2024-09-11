2185bdb4ab09bc48022ea8ff78f72215ed532084
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-09-10 23:42:18 -0700

    Delete achievement2-practice directory

--- achievement2-practice/exercise_4/A2_Recipe_App/src/recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home
-
-
-app_name = 'recipes'
-
-urlpatterns = [
-    path('', home, name='home'),
-]
--- achievement2-practice/exercise_5/A2_Recipe_App/src/recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('recipes/', views.RecipeListView.as_view(), name='recipe_list'),
-    path('recipe/<uuid:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
-]
